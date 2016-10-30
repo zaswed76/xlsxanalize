de1a13b27dfc0f2361a88ae6e9e2e3878d4cd555
serg <zaswed76@gmail.com>
2016-10-30 22:37:55 +0200

    class File

--- xlsxanalize/analizator.py
+++ xlsxanalize/analizator.py
@@ -1,25 +1,65 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 from openpyxl import load_workbook
 
+PATTERN = re.compile(r'\D')
 
-def list_paths(direct):
-    return [os.path.join(direct, x) for x in os.listdir(direct)]
 
-def read_dir(dir):
-    months = []
-    for root, dirs, files in os.walk(
-            dir):  # пройти по директории рекурсивно
+class File:
+    def __init__(self, root, name, ext, pat):
+        self._date_parse_flag = True
+        self.ext = ext
+        self.name = name
+        self.root = root
+        self.fullname = os.path.join(self.root, self.name, self.ext)
+        try:
+            self.day_month, self.month, _, _, self.year, self.time = File.pars_line(
+                self.name, pat)
+        except ValueError:
+            self._date_parse_flag = False
+
+    def __repr__(self):
+        return self.name
+
+    @property
+    def date_parse_flag(self):
+        return self._date_parse_flag
+
+    @date_parse_flag.setter
+    def date_parse_flag(self, flag):
+        self._date_parse_flag = flag
+
+
+
+    @staticmethod
+    def pars_line(line, pat):
+        """
+
+        :param line: str
+        :param pat: re
+        :return: [date, month, date, month, year, time]
+        """
+        result = re.split(pat, line)
+        return [x for x in result if x]
+
+
+def read_dir(dir, default_ext='.xlsx'):
+    path_lst = []
+    for root, dirs, files in os.walk(dir):
         for name in files:
-            fullname = os.path.join(root,
-                                    name)  # получаем полное имя файла
-            if dirs:
-                months.extend(dirs)
-    return months
+            name, ext = os.path.splitext(name)
+            if ext == default_ext:
+                path_lst.append(File(root, name, ext, PATTERN))
+    return path_lst
 
-            # os.path.getctime(fullname)  # делаем что-нибудь с ним
 
 if __name__ == '__main__':
-    m = read_dir(r'E:\SERG\programming\xlsxa\xlsxanalize\xlsxanalize\data\Отчёты по Принтеру')
-    print(m[0])
+    m = read_dir(
+        r'D:\0SYNC\python_projects\xlsxanalize\xlsxanalize\data\Отчёты по Принтеру')
+    print(m[0].name)
+    print(m[0].day_month)
+    print(m[0].year)
+    print(m[0].time)
+    print(m[0].date_parse_flag)
