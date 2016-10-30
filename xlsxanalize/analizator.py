c5fa082e6ec053f1e3e7d654258e60fa8394e534
serg <zaswed76@gmail.com>
2016-10-31 00:32:44 +0200

    class File

--- xlsxanalize/analizator.py
+++ xlsxanalize/analizator.py
@@ -5,19 +5,21 @@ import re
 from openpyxl import load_workbook
 
 PATTERN = re.compile(r'\D')
+EXT = '.xlsx'
 
 
 class File:
-    def __init__(self, root, name, ext, pat):
+    def __init__(self, root='', name='', ext='', pat=''):
         self._date_parse_flag = True
-        self.ext = ext
+        # self.ext = ext
         self.name = name
         self.root = root
-        self.fullname = os.path.join(self.root, self.name, self.ext)
+        self.fullname = os.path.join(self.root, self.name)
         try:
             self.day_month, self.month, _, _, self.year, self.time = File.pars_line(
                 self.name, pat)
         except ValueError:
+            self.day_month, self.month, _, _, self.year, self.time = '?' * 6
             self._date_parse_flag = False
 
     def __repr__(self):
@@ -56,10 +58,15 @@ def read_dir(dir, default_ext='.xlsx'):
 
 
 if __name__ == '__main__':
-    m = read_dir(
-        r'D:\0SYNC\python_projects\xlsxanalize\xlsxanalize\data\Отчёты по Принтеру')
-    print(m[0].name)
-    print(m[0].day_month)
-    print(m[0].year)
-    print(m[0].time)
-    print(m[0].date_parse_flag)
+    # m = read_dir(
+    #     r'D:\0SYNC\python_projects\xlsxanalize\xlsxanalize\data\Отчёты по Принтеру')
+    # print(m[0].name)
+    # print(m[0].day_month)
+    # print(m[0].year)
+    # print(m[0].time)
+    # print(m[0].date_parse_flag)
+    p = File(ext=EXT, name='30.01-31.01.2016(24).xlsx', pat=PATTERN)
+    print(p.name)
+    print(p.fullname)
+    print(p.day_month)
+    print(p.date_parse_flag)
