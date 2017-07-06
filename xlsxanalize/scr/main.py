be1723bef8580da0f8ae1d02f4efaac36ee15e67
Cassa <zaswed76@gmail.com>
2017-07-06 20:34:54 +0300

    ex4

--- xlsxanalize/scr/main.py
+++ xlsxanalize/scr/main.py
@@ -1,11 +1,19 @@
 
 
-from xlsxanalize.scr import mess
+from xlsxanalize.scr import mess, mailpy
 
-xlsx_data = ['add_income_mess', 'admin_income', 'all_expenses',
+xlsx_data_list = ['add_income_mess', 'admin_income', 'all_expenses',
              'bar_income', 'change_money', 'change_money_expenses',
              'expenses_mess', 'salary_mess', 'total_in_safe',
              'total_income', 'z_report']
 
-message =  mess.Message()
-message.register_xlsx_data(*xlsx_data)
+mymail = "zaswed76@gmail.com"
+tomail = "sergitland@gmail.com"
+subj = "тема"
+
+ms =  mess.Message("../templates", "mess.html")
+ms.register_xlsx_data(*xlsx_data_list)
+ms_text = ms.text()
+psw = "5422717fasad"
+
+mailpy.run_mail(mymail, [tomail], subj, ms_text, psw)
