f8c015994578225b6c7d36c221cafdc23a791e92
Cassa <zaswed76@gmail.com>
2017-07-06 15:38:57 +0300

    ex2

---
+++ xlsxanalize/scr/xlsx_data.py
@@ -0,0 +1,18 @@
+
+def z_report():
+    return 5000
+
+def bar_income():
+    return 100
+
+def add_income_mess():
+    return None
+
+def admin_income():
+    return 0
+
+def change_money():
+    return 100
+
+def total_income():
+    return 5200
